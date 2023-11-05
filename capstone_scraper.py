caf454dd0ee5108da7f4b25d28d096fd972f8c72
Tatwamasi Mishra <72276849+tatwamasidotexe@users.noreply.github.com>
2023-11-05 17:17:08 +0530

    added likes and date posted attribute scraping

--- capstone_scraper.py
+++ capstone_scraper.py
@@ -8,8 +8,8 @@ from openpyxl import load_workbook
 class Review:
     username: str = None
     star_rating: int = None
-    #thumbsup_count: int = None
-    #rating_time: str = None
+    likes: int = None
+    date_posted: str = None
     review: str = None
 
 @dataclass
@@ -28,17 +28,8 @@ class ReviewList :
         df = self.dataframe()
         file = f"./capstone/{filename}.xlsx"
 
-        # workbook = load_workbook(file)
-        # writer = pd.ExcelWriter(file, engine='openpyxl')
-        # writer.book = workbook
-        # writer.sheets = {ws.title: ws for ws in workbook.worksheets}
-
-        # df.to_excel(writer, startrow=writer.sheets['Sheet1'].max_row, index = False, header= False)
-        # writer.close()
-
         with pd.ExcelWriter(file, mode='a', engine='openpyxl', if_sheet_exists='overlay') as writer :
             df.to_excel(writer, sheet_name='Sheet1', startrow=writer.sheets['Sheet1'].max_row, index=False, header=False)
-            # writer.close()
 
 def main():
     with sync_playwright() as p:
@@ -47,7 +38,7 @@ def main():
         
         #open browser window
         page.goto("https://play.google.com/store", timeout=60000)
-        search_for = f"paytm" 
+        search_for = f"phonepe" 
         
         page.locator('//button[@class="VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ mN1ivc"]').click()
         page.locator('//input[@class="HWAcU"]').fill(search_for)
@@ -60,11 +51,12 @@ def main():
         page.wait_for_timeout(3000)
 
         cnt = -1
-        for x in range(35) :
+        for x in range(30) : 
             
             if cnt >= 10000 : break
 
             print(f"====================================== SCROLL RUN {x+1} ========================================")
+            
             #scroll
             page.hover('//div[@class="odk6He"]')
             for _ in range(25):
@@ -74,16 +66,19 @@ def main():
             username_xpath = '//header[@class="c1bOId"]/div[@class="YNR7H"]/div[@class="gSGphe"]/div[@class="X5PpBb"]'
             review_xpath = '//div[@class="h3YV2d"]'
             star_count_xpath = '//header[@class="c1bOId"]/div[2]/div/span/span[@class="Z1Dz7b"]'
-
+            date_xpath = '//header[@class="c1bOId"]/div[2]/span'
+            likes_xpath = '//div[2]/div[@class="AJTPZc"]'
 
             rl = ReviewList()        
-            reviewBox_xpath = '//div[@class="odk6He"]/div/div[@class="RHo1pe"]'        
+            reviewBox_xpath = '//div[@class="odk6He"]/div/div[@class="RHo1pe"]'
+                   
             reviewsList = page.locator(reviewBox_xpath).all()
             print(len(reviewsList))
 
             cnt +=1
             for j in range(cnt, len(reviewsList)):
                 reviewNode = Review()
+                
                 #username
                 print(f"REVIEW #{j+1}")
                 try:
@@ -93,6 +88,22 @@ def main():
                 except:
                     reviewNode.username = None
 
+                # star count
+                try :
+                    nodes = reviewsList[j].locator(star_count_xpath).all()
+                    reviewNode.star_rating = len(nodes)
+                    print(f"STAR RATING = {reviewNode.star_rating}")
+                except : 
+                    reviewNode.star_rating = None
+
+                # date posted
+                try :
+                    node = reviewsList[j].locator(date_xpath)
+                    reviewNode.date_posted = pd.to_datetime(node.inner_text()).strftime("%Y/%m/%d")
+                    print(f"DATE POSTED = {reviewNode.date_posted}")
+                except : 
+                    reviewNode.date_posted = None
+
                 #review
                 try:
                     node = reviewsList[j].locator(review_xpath)
@@ -101,27 +112,29 @@ def main():
                 except:
                     reviewNode.review = None
                     print(reviewNode.review)
-                
-                # star count
-                try :
-                    nodes = reviewsList[j].locator(star_count_xpath).all()
-                    reviewNode.star_rating = len(nodes)
-                    print(f"STAR RATING = {reviewNode.star_rating}\n")
-                except : 
-                    reviewNode.star_rating = None
 
+                try:
+                    text = (reviewsList[j].locator(likes_xpath)).element_handle().inner_text()
+                    l = int(text.split(" ")[0].replace(',', ""))
+                    print(f"LIKES = {l}")
+                    reviewNode.likes = l
+                except:
+                    reviewNode.likes = None
+                    
+
+                print()
                 rl.review_list.append(reviewNode)
                 cnt = j
 
-            rl.save_to_excel("paytm")     
+            rl.save_to_excel("phonepe")     
 
         browser.close()
 
         # cleaning up the data file - adding headers
-        df = pd.read_excel('./capstone/paytm.xlsx')
-        headers = ['username', 'star_rating', 'review']
+        df = pd.read_excel('./capstone/phonepe.xlsx')
+        headers = ['username', 'star_rating', 'likes', 'date_posted', 'review']
         df.columns = headers
-        df.to_excel('./capstone/paytm.xlsx',index=False)
+        df.to_excel('./capstone/phonepe.xlsx',index=False)
 
 if __name__ == "__main__":
 
@@ -135,7 +148,7 @@ if __name__ == "__main__":
     else:
         # in case no arguments passed:
         # scraper will search for this on Google Maps
-        search_for = 'paytm'
+        search_for = 'phonepe'
     
 
 main()
